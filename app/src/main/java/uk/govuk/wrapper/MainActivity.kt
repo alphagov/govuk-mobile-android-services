b6c2beebb3143222ef7b05781ac9994f78ac28e7
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-05-03 11:57:32 +0100

    Add services module

--- app/src/main/java/uk/govuk/wrapper/MainActivity.kt
+++ app/src/main/java/uk/govuk/wrapper/MainActivity.kt
@@ -3,6 +3,7 @@ package uk.govuk.wrapper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import uk.govuk.services.ServicesStart
 import uk.govuk.wrapper.ui.theme.GovukmobileandroidservicesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("GOV.UK Android Services")
+                    Column {
+                        Greeting("GOV.UK Android Wrapper")
+                        ServicesStart()
+                    }
                 }
             }
         }
@@ -41,6 +46,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GovukmobileandroidservicesTheme {
-        Greeting("GOV.UK Android Services")
+        Greeting("GOV.UK Android Wrapper")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "govuk-mobile-android-services"
 include(":app")
- 
+include(":services")
